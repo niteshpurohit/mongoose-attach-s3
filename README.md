fb962ac2c9025b78dba352f652eb592ebf900e49
Nitesh Purohit <nitesh@awrtechnologies.com>
2017-12-10 14:36:31 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mongoose-attach-s3
+mongoose-attach StorageProvider that uploads files to Amazon S3
---
+++ libs/S3.js
@@ -0,0 +1,7 @@
+/*jslint node: true */
+'use strict';
+
+const path = require('path');
+const url = require('url');
+const knox = require('knox-s3');
+const check = require('check-types');
